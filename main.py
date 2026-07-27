38e7ccebb60399cf84dde77ca2ae19298a695dc3
Mayank-Kumar13 <mayanktomer1306@gmail.com>
2026-07-27 22:26:11 +0530

    extra

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
-
+from ocr import get_ocr_text
 load_dotenv()
-
 model = ChatMistralAI(
     model="mistral-large-latest"
 )
-
-answer = model.invoke("Hello! Who are you?")
+pdf_path = "Unit 3 sem 1.pdf"
+ocr_docs = get_ocr_text(pdf_path)
+answer = model.invoke(f"explain Thermodynamic Equilibrium \n\n{ocr_docs}")
 print(answer.content)
--- ocr.py
+++ ocr.py
@@ -3,31 +3,45 @@ from dotenv import load_dotenv
 from mistralai.client import Mistral
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 load_dotenv()
+
 client = Mistral(api_key=os.getenv("MISTRAL_API_KEY"))
-uploaded_file = client.files.upload(
-    file={
-        "file_name": "Unit 3 sem 1.pdf",
-        "content": open("Unit 3 sem 1.pdf", "rb"),
-    },
-    purpose="ocr",
-)
-signed_url = client.files.get_signed_url(file_id=uploaded_file.id)
-ocr_response = client.ocr.process(
-    model="mistral-ocr-latest",
-    document={
-        "type": "document_url",
-        "document_url": signed_url.url,
-    },
-)
-text = ""
-for page in ocr_response.pages:
-    text += page.markdown + "\n"
-documents = [Document(page_content=text)]
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200,
-)
-chunks = text_splitter.split_documents(documents)
-for i in range(3):
-    print(chunks[i].page_content)
+
+def get_ocr_text(pdf_path):
+    uploaded_file = client.files.upload(
+        file={
+            "file_name": os.path.basename(pdf_path),
+            "content": open(pdf_path, "rb"),
+        },
+        purpose="ocr",
+    )
+
+    signed_url = client.files.get_signed_url(file_id=uploaded_file.id)
+
+    ocr_response = client.ocr.process(
+        model="mistral-ocr-latest",
+        document={
+            "type": "document_url",
+            "document_url": signed_url.url,
+        },
+    )
+
+    text = ""
+    for page in ocr_response.pages:
+        text += page.markdown + "\n"
+
+    documents = [Document(page_content=text)]
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+
+    chunks = text_splitter.split_documents(documents)
+
+    ocr_docs = ""
+    for chunk in chunks:
+        ocr_docs += chunk.page_content
+
+    return ocr_docs
